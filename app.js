c76ce4440b28c34f18de91365c6b17c7b509b99f
Jon <jonathan.tan83@gmail.com>
2015-03-02 18:26:04 -1000

    added post and get to add and access todos in db

--- app.js
+++ app.js
@@ -20,19 +20,39 @@ app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.set('view engine','jade');
 
-app.get('/', function(req, res){
-  res.render('index');
+app.get('/', function (req, res){
+  Todo.find(function (err, todos){
+    console.log('todo list: ' + todos);
+    if (err) throw err;
+    res.render('index', { 
+      todos : todos 
+    });
+  });
+  // res.render('index', { title : title, description : description });
+
 });
 
 app.get('/new_todo', function(req, res){
   res.render('new_todo');
 });
 
-app.get('/edit', function(res, res){
+app.get('/edit', function(req, res){
   res.render('edit');
 });
 
-
+app.post('/new_todo', function(req, res){
+  console.log(req.body);
+  var newTodo = new Todo({
+    title : req.body.title,
+    description: req.body.description, 
+    is_done : false,
+    created_at : new Date()
+  });
+  newTodo.save(function(err){
+    if (err) throw err;
+    console.log("saved!");
+  });
+});
 
 var server = app.listen(3000, function () {
 
